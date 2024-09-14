d33326423ed7e4e3de20dce48c41f09c90fb653b
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2024-09-14 23:57:16 +0700

    Show total score in leaderboard

--- src/components/module/leaderboard/Leaderboard.tsx
+++ src/components/module/leaderboard/Leaderboard.tsx
@@ -82,7 +82,7 @@ export default function Leaderboard({ isAdmin, className }: LeaderboardProps) {
                   <td>
                     <div className="flex flex-col gap-2">
                       <strong className="font-bold">{team.name}</strong>
-                      {/* <span title="Total Score">{team.total_score.toFixed(2)}</span> */}
+                      <span title="Total Score">{team.total_score.toFixed(2)}</span>
                     </div>
                   </td>
 
